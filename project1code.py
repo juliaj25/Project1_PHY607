83e73af1a50a277d0c05b0aea990d71a89f1a658
Julia Jones <jjone112@syr.edu>
2025-10-13 19:55:28 -0400

    RK4 method

--- project1code.py
+++ project1code.py
@@ -34,6 +34,24 @@ def euler_solver(f, t_span, y0, dt):
         y_values[i] = euler_step(f, t_values[i-1], y_values[i-1], dt)
     return t_values, y_values
 
+# Runge-Kutta 4th order implementation
+def rk4_step(f, t, y, dt):
+    k1 = f(t, y)
+    k2 = f(t + dt/2, y + dt/2 * k1)
+    k3 = f(t + dt/2, y + dt/2 * k2)
+    k4 = f(t + dt, y + dt * k3)
+    return y + (dt/6) * (k1 + 2*k2 + 2*k3 + k4)
+
+def rk4_solver(f, t_span, y0, dt):
+    t_values = np.arange(t_span[0], t_span[1] + dt, dt)
+    y_values = np.zeros((len(t_values), len(y0)))
+    y_values[0] = y0
+    
+    for i in range(1, len(t_values)):
+        y_values[i] = rk4_step(f, t_values[i-1], y_values[i-1], dt)
+    return t_values, y_values
+
+
 
 # Initial conditions
 y0 = np.array([1.0, 0.0])  # initial displacement and velocity
@@ -43,11 +61,20 @@ dt = 0.05                  # time step
 # Solve using Euler
 t_euler, y_euler = euler_solver(damped_oscillator, t_span, y0, dt)
 
+
+# Solve using RK4
+t_rk4, y_rk4 = rk4_solver(damped_oscillator, t_span, y0, dt)
+
+# Solve using SciPy's solve_ivp 
+sol = solve_ivp(damped_oscillator, t_span, y0, t_eval=np.arange(t_span[0], t_span[1]+dt, dt))
+
 # Plot results
 plt.figure(figsize=(12, 6))
 
 plt.plot(t_euler, y_euler[:, 0], 'r--', label='Euler method')
 
+plt.plot(t_rk4, y_rk4[:, 0], 'b-.', label='RK4 method')
+plt.plot(sol.t, sol.y[0], 'k', label='SciPy solve_ivp ')
 
 plt.xlabel('Time [s]')
 plt.ylabel('Displacement [x]')
